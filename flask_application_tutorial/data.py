a3d9c45c41f6d05a50d167e66222c350e563ef03
Dragos Manea <maneadr@gmail.com>
2020-08-25 18:19:06 +0300

    created data file for articles, articles.html and article.html.

---
+++ flask_application_tutorial/data.py
@@ -0,0 +1,25 @@
+def articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Article One",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Repellendus quasi, accusantium aspernatur unde aut alias et obcaecati necessitatibus tempore autem! Repudiandae soluta fuga eligendi tempora. Quod facilis odio quos cumque?",
+            "author": "Tata John",
+            "create_date": "25-08-2020",
+        },
+        {
+            "id": 2,
+            "title": "Article Two",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Repellendus quasi, accusantium aspernatur unde aut alias et obcaecati necessitatibus tempore autem! Repudiandae soluta fuga eligendi tempora. Quod facilis odio quos cumque?",
+            "author": "Tata John",
+            "create_date": "25-08-2020",
+        },
+        {
+            "id": 3,
+            "title": "Article Three",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Repellendus quasi, accusantium aspernatur unde aut alias et obcaecati necessitatibus tempore autem! Repudiandae soluta fuga eligendi tempora. Quod facilis odio quos cumque?",
+            "author": "Tata John",
+            "create_date": "25-08-2020",
+        },
+    ]
+    return articles
