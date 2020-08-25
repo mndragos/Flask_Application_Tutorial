398eb89c2cc118f6e3202bcca697067f2d7e55b8
Dragos Manea <maneadr@gmail.com>
2020-08-25 16:22:18 +0300

    deleted comments in app.py

--- flask_application_tutorial/app.py
+++ flask_application_tutorial/app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# render_template priveste in folderul templates.
-# home.html extends layout.html, astfel foloseste acelasi format al paginii
-# ori de cate ori este nevoie.
-
 
 @app.route("/")
 def index():
