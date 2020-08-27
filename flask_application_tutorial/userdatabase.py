93f30a9be27f1e53a84d73dc5c6f5ad8e78ade28
Dragos Manea <maneadr@gmail.com>
2020-08-27 18:37:00 +0300

    changed  register_date column in db's table to show local time.

--- flask_application_tutorial/userdatabase.py
+++ flask_application_tutorial/userdatabase.py
@@ -14,13 +14,17 @@ c.execute(
     email VARCHAR(100),
     username VARCHAR(30),
     password VARCHAR(100),
-    register_date TIMESTAMP DEFAULT CURENT_TIMESTAMP
+    register_date DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now', 'localtime'))
 )
 """
 )
 
 # commit changes
 conn.commit()
+
+# c.execute("SELECT * FROM users")
+
+# print(c.fetchall())
 
 # allways close connection
 conn.close()
